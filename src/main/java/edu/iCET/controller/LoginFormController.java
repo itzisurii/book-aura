5cbda5bee823cc62e581868a3d04ade1829cd1e3
itzisurii <itzIsurika08@gmail.com>
2025-12-03 23:59:46 +0530

    Add login logic for Customer with scene switching and error alert

--- src/main/java/edu/iCET/controller/LoginFormController.java
+++ src/main/java/edu/iCET/controller/LoginFormController.java
@@ -3,9 +3,15 @@ package edu.iCET.controller;
 import com.jfoenix.controls.JFXComboBox;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ComboBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 
 public class LoginFormController {
@@ -24,8 +30,41 @@ public class LoginFormController {
         cmbUserType.getItems().addAll("Customer", "Staff");
     }
 
+    Stage stage = new Stage();
+
     @FXML
     void loginOnAction(ActionEvent event) {
+        String userType = cmbUserType.getValue();
+        String username = txtUsername.getText();
+        String password = txtPassword.getText();
+
+        if(userType == null){
+            System.out.println("Select user type!");
+            return;
+        }
+
+        switch(userType){
+            case "Customer":
+                if (username.equals("isu") && password.equals("1234")){
+                    try {
+                        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/customer_dashboard.fxml"))));
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    stage.show();
+                    txtUsername.clear();
+                    txtPassword.clear();
+                }else{
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setTitle("Login Failed");
+                    alert.setHeaderText("Invalid Credentials");
+                    alert.setContentText("Username or Password is incorrect.");
+                    alert.show();
+                }
+                break;
+            case "Staff":
+
+        }
 
     }
 }
