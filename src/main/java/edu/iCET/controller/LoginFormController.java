0da8333cb855b8e051c030497c12bcadd42c0aad
itzisurii <itzIsurika08@gmail.com>
2025-12-03 21:36:02 +0530

    Add LoginFormController with UI bindings and event handler structure

--- src/main/java/edu/iCET/controller/LoginFormController.java
+++ src/main/java/edu/iCET/controller/LoginFormController.java
@@ -1,5 +1,31 @@
 package edu.iCET.controller;
 
+import com.jfoenix.controls.JFXComboBox;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ComboBox;
+
+
 public class LoginFormController {
 
+    @FXML
+    private JFXComboBox<?> cmbUserType;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    public void initialize() {
+
+    }
+
+    @FXML
+    void loginOnAction(ActionEvent event) {
+
+    }
 }
