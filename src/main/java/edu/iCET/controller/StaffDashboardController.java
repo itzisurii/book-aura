1f69b95325a4945945814f0e0388bf33788562e5
itzisurii <itzIsurika08@gmail.com>
2025-12-09 23:50:51 +0530

    Add new UI components and layout structure to staff_dashboard.fxml

--- src/main/java/edu/iCET/controller/StaffDashboardController.java
+++ src/main/java/edu/iCET/controller/StaffDashboardController.java
@@ -1,4 +1,23 @@
 package edu.iCET.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class StaffDashboardController {
+
+    @FXML
+    void browseBooksOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void homeOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void myRentalOnAction(ActionEvent event) {
+
+    }
+
 }
