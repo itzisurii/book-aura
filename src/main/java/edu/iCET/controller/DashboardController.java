259fffeaaf547d16111e0718eed475d1e0cf8859
itzisurii <itzIsurika08@gmail.com>
2026-01-08 22:14:59 +0530

    Create DashboardController to manage dashboard labels

--- src/main/java/edu/iCET/controller/DashboardController.java
+++ src/main/java/edu/iCET/controller/DashboardController.java
@@ -1,4 +1,24 @@
 package edu.iCET.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
 public class DashboardController {
+
+    @FXML
+    private AnchorPane contentPane;
+
+    @FXML
+    private Label customerId;
+
+    @FXML
+    private Label employeeId;
+
+    @FXML
+    private Label stocksId;
+
+    @FXML
+    private Label todayId;
+
 }
