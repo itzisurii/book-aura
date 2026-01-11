10685ba92e2e8c81242f881b3c4bd0d5e0879cac
itzisurii <itzIsurika08@gmail.com>
2026-01-11 13:13:01 +0530

    Defined empty @FXML methods for dashboard, books, customers, rentals, returns, reports, and logout actions

---
+++ src/main/java/edu/iCET/controller/AdminDashboardController.java
@@ -0,0 +1,56 @@
+
+package edu.iCET.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AdminDashboardController {
+
+    @FXML
+    private AnchorPane contentPane;
+
+    @FXML
+    void booksOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void customersOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void dashboardOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logoutOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void rentalsOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void reportsOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void returnsOnAction(ActionEvent event) {
+
+    }
+
+
+
+}
+
