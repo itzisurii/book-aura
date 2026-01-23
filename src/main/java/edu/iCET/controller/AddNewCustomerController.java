40aa56a853fa4bb1f057b905fc7a98e66f4fbfbe
itzisurii <itzIsurika08@gmail.com>
2026-01-23 15:45:10 +0530

    Prepares controller for handling new customer addition

---
+++ src/main/java/edu/iCET/controller/AddNewCustomerController.java
@@ -0,0 +1,37 @@
+package edu.iCET.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class AddNewCustomerController {
+
+    @FXML
+    private ImageView imgTitle;
+
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private TextField txtID;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtPhone;
+
+    @FXML
+    private TextField txtTitle;
+
+
+    @FXML
+    void btnAddCustomer(ActionEvent event) {
+
+    }
+
+}
