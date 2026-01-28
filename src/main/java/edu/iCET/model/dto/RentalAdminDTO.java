1ad5c63711d5fb19ccc3349c39e6b058c9343483
itzisurii <itzIsurika08@gmail.com>
2026-01-28 23:53:46 +0530

    Add RentalAdmin dto

--- src/main/java/edu/iCET/model/dto/RentalAdminDTO.java
+++ src/main/java/edu/iCET/model/dto/RentalAdminDTO.java
@@ -1,4 +1,18 @@
 package edu.iCET.model.dto;
 
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class RentalAdminDTO {
+    private String rentalId;
+    private String bookId;
+    private String customerId;
+    private LocalDate issueDate;
+    private LocalDate dueDate;
 }
