723f53e847c6635da42096b871a753fd5e393a3e
itzisurii <itzIsurika08@gmail.com>
2026-01-28 23:53:28 +0530

    Add RentalAdmin entity

--- src/main/java/edu/iCET/model/entity/RentalAdmin.java
+++ src/main/java/edu/iCET/model/entity/RentalAdmin.java
@@ -1,4 +1,23 @@
 package edu.iCET.model.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
 public class RentalAdmin {
+
+    @Id
+    private String rentalId;
+    private String bookId;
+    private String customerId;
+    private LocalDate issueDate;
+    private LocalDate dueDate;
 }
