efb2147194e7b01959522203cd07a8bccad275bc
itzisurii <itzIsurika08@gmail.com>
2026-02-02 15:43:25 +0530

    added Customer cards

---
+++ src/main/java/edu/iCET/controller/CustomerCardsAdmin.java
@@ -0,0 +1,18 @@
+package edu.iCET.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class CustomerCardsAdmin {
+
+    @FXML
+    private ImageView imgTitle;
+
+    @FXML
+    private Label lblName;
+
+    @FXML
+    private Label lblPhoneNumber;
+
+}
