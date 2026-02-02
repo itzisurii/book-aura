7c2b62ac48e41b4d190f49376f1a63c6dff6664f
itzisurii <itzIsurika08@gmail.com>
2026-02-02 17:16:17 +0530

    add customer admin controller and dynamic customer card loading

--- src/main/java/edu/iCET/controller/CustomerAdminController.java
+++ src/main/java/edu/iCET/controller/CustomerAdminController.java
@@ -1,16 +1,28 @@
 package edu.iCET.controller;
 
 import com.jfoenix.controls.JFXComboBox;
+import edu.iCET.model.dto.CustomerAdminDTO;
+import edu.iCET.service.CustomerAdminServiceImpl;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
-public class CustomerAdminController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CustomerAdminController implements Initializable {
+
+    CustomerAdminServiceImpl customerAdminService = new CustomerAdminServiceImpl();
 
     @FXML
-    private JFXComboBox<?> cmbTitle;
+    private JFXComboBox<String> cmbTitle;
 
     @FXML
     private AnchorPane contentPane;
@@ -33,10 +45,6 @@ public class CustomerAdminController {
     @FXML
     private TextField txtPhoneNumber;
 
-    @FXML
-    void addCustomerOnAction(ActionEvent event) {
-
-    }
 
     @FXML
     void deleteOnAction(ActionEvent event) {
@@ -46,6 +54,41 @@ public class CustomerAdminController {
     @FXML
     void updateOnAction(ActionEvent event) {
 
+    }
+
+    @FXML
+    void addCustomerOnAction(ActionEvent event) {
+
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        cmbTitle.getItems().addAll("Mr","Mrs","Miss");
+        loadCustomers();
+        lblID.setText(customerAdminService.generateCustomerId());
+    }
+
+    private void loadCustomers(){
+        contentPane.getChildren().clear();
+        double y = 10;
+
+        for (CustomerAdminDTO customer : customerAdminService.getAllCustomers()) {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/customer_cards_admin.fxml"));
+                AnchorPane card = loader.load();
+
+                CustomerCardsAdmin controller = loader.getController();
+                controller.setData(customer);
+
+                card.setLayoutY(y);
+                y += 110;
+
+                contentPane.getChildren().add(card);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
