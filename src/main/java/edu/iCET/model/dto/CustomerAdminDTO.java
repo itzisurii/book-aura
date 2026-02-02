2ddd053f630f5bb36b11519d161cd4a433189259
itzisurii <itzIsurika08@gmail.com>
2026-02-02 15:37:49 +0530

    added CustomerAdminDTO

--- src/main/java/edu/iCET/model/dto/CustomerAdminDTO.java
+++ src/main/java/edu/iCET/model/dto/CustomerAdminDTO.java
@@ -1,4 +1,16 @@
 package edu.iCET.model.dto;
 
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Setter
+@Getter
 public class CustomerAdminDTO {
+    private String customerId;
+    private String title;
+    private String name;
+    private String phone;
+    private String email;
 }
