0632618f2ee78f5d716a67540cb64a66dfec121c
itzisurii <itzIsurika08@gmail.com>
2026-02-09 11:51:06 +0530

    Implement customer data binding in admin view

--- src/main/java/edu/iCET/controller/CustomerCardsAdmin.java
+++ src/main/java/edu/iCET/controller/CustomerCardsAdmin.java
@@ -1,11 +1,13 @@
 package edu.iCET.controller;
 
+import edu.iCET.model.dto.CustomerAdminDTO;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 
 public class CustomerCardsAdmin {
 
+    private CustomerAdminDTO customerAdminDTO;
     @FXML
     private ImageView imgTitle;
 
@@ -15,4 +17,15 @@ public class CustomerCardsAdmin {
     @FXML
     private Label lblPhoneNumber;
 
+
+    public void setData(CustomerAdminDTO customer) {
+        this.customerAdminDTO = customer;
+
+        lblName.setText(customer.getTitle() + " " + customer.getName());
+        lblPhoneNumber.setText(customer.getPhone());
+    }
+
+    public CustomerAdminDTO customerAdminDTO(){
+        return customerAdminDTO;
+    }
 }
